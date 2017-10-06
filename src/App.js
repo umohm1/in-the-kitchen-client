09b5f8defed52a708c731c7cb1bafc069cdda34a
Mary <mumoh@mozilla.com>
2017-10-06 17:56:46 -0400

    Complete connection to API backend

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import Recipes from './containers/Recipes';
 import './App.css';
 
-const recipes = []
+const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/recipes`)
+      .then(res => res.json())
+      .then(res => this.setState {recipes}))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Recipes recipes={recipes} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
