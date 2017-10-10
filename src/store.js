087c0309312699269e54e09229cfce049a5b2c95
Mary <mumoh@mozilla.com>
2017-10-10 17:40:01 -0400

    Continue building out store.js

--- src/store.js
+++ src/store.js
@@ -3,3 +3,26 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux';
+import thunk from 'redux-thunk';
+
+const recipesReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_RECIPES':
+    return action.recipes;
+
+    default:
+    reutrn state;
+  }
+}
+
+const reducers = combineReducers({
+  recipes: recipesReducer
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
