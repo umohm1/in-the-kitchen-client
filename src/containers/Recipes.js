6ca0148071b53e435d6ef566dc04fd1dfb5fef63
Mary <mumoh@mozilla.com>
2017-10-10 14:55:42 -0400

    Add basic styling

--- src/containers/Recipes.js
+++ src/containers/Recipes.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Recipes.css'
 
 const Recipes = (props) => (
   <div>
   <h1>Recipes</h1>
     {props.recipes.map(recipe =>
       <div key={recipe.id} className="RecipeCard">
-      <h2>{recipe.title}</h2>
+      <p>Name: {recipe.name}</p>
       <p>Ingredients: {recipe.ingredients}</p>
       <p>Directions: {recipe.directions}</p>
       <p>Cook Time: {recipe.cook_time}</p>
